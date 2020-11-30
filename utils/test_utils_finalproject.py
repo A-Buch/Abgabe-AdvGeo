366842585d26ead7e19204069a6c2c6c46968ddc
Anna <a.buch@stud.uni-heidelberg.de>
2020-12-01 00:56:05 +0100

    final

--- utils/test_utils_finalproject.py
+++ utils/test_utils_finalproject.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
 # import unittest
 # class MyTestCase(unittest.TestCase):
 #	  def test_something(self):
@@ -11,7 +15,58 @@
 import os
 import pandas as pd
 import numpy as np
+from wetterdienst.dwd.observations import DWDObservationMetadata
+
 from utils_finalproject import *
+
+
+
+def test_get_weatherdata():
+	"""
+	-- check how NA values are expressed and if weatherparameters are in appropiate units---
+	"""
+
+	# when 
+	station_id = [4928] ## Stuttgart Schnarrenberg
+	weather_parameters = [DWDObservationParameterSet.PRECIPITATION,
+			DWDObservationParameterSet.TEMPERATURE_AIR,
+			DWDObservationParameterSet.WIND,
+			DWDObservationParameterSet.PRESSURE] 
+	resolution = DWDObservationResolution.HOURLY
+	periods = [DWDObservationPeriod.RECENT]
+	start_date = "2019-12-21"
+	end_date = "2019-12-31"
+	weather_df = get_weatherdata(station_id, weather_parameters, resolution, periods, start_date, end_date)
+
+	exp_NA_value = str("[<NA>]")
+	NA_value = observations.isin([exp_NA_value]).any()
+
+	# from CDC Portal: it is known that the 29th Dec 2019 has a NA value
+	NA_value = weather_df.loc[(weather_df.DATE == "2019-12-29") & (weather_df.ELEMENT=="PRECIPITATION_FORM")]
+	NA_value = str(NA_value.VALUE.values)
+
+	assert NA_value == exp_NA_value
+
+
+	## unit test for the most common weather parameters 
+	parameter_units = weather_df[["ELEMENT", "VALUE"]]
+	parameter_units = parameter_units.pivot_table(index=parameter_units.index, columns="ELEMENT", values="VALUE", aggfunc="first")
+
+	# get measurement values for each parameter and delete NaNs due to check if values are in certain unit range
+	param_temperature = parameter_units.loc[:,"TEMPERATURE_AIR_200"]
+	param_temperature = param_temperature[param_temperature.notna()]
+	param_precipitation = parameter_units.loc[:,"PRECIPITATION_HEIGHT"]
+	param_precipitation = param_precipitation[param_precipitation.notna()]
+	param_wind_speed = parameter_units.loc[:,"WIND_SPEED"]
+	param_wind_speed = param_wind_speed[param_wind_speed.notna()]
+	param_wind_direction = parameter_units.loc[:,"WIND_DIRECTION"]
+	param_wind_direction= param_wind_direction[param_wind_direction.notna()]
+
+	assert param_temperature.between(-20.0, 10.0).all() # check if in °C
+	assert param_precipitation.between(0, 1000.0).all() # check if in mm (hourly-annual)
+	assert param_wind_speed.between(0, 70.0).all() # check if in m/s
+	assert param_wind_direction.between(0.0, 360.0).all() # check if in degrees (between 0° and 360°)
+
 
 
 
--- utils/utils_finalproject.py
+++ utils/utils_finalproject.py
@@ -10,15 +10,16 @@ import pandas as pd
 from datetime import datetime, date
 from wetterdienst.dwd.observations import DWDObservationData, DWDObservationParameterSet, DWDObservationPeriod, DWDObservationResolution
 
-
-import pdb
-
+from sklearn.svm import SVR, SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import mean_squared_error
 
 
 def get_weatherdata(station_id, weather_parameters, resolution, periods, start_date, end_date):
 	"""
 	--- Return timeseries of DWD weatherstations with userdefined parameters, spatial and temporal resoultion ---- 
-	station_id : list of station number eg. [4928]
+	station_id : list of station number(s) eg. [4928]
 	weather_parameters: list of parameters in format: DWDObservationParameterSet.PARAMETERNAME,
 	resolution :  resolution time in format: DWDObservationResolution.RESOLUTION
 	periods : list of observation period in format: DWDObservationPeriod.PERIOD
@@ -26,10 +27,11 @@ def get_weatherdata(station_id, weather_parameters, resolution, periods, start_d
 	end_date : string indicating the end date in format "YYYY-MM-DD"
 	return pandas DataFrame with selected observations
 	
-	Further information: https://wetterdienst.readthedocs.io/_/downloads/en/latest/pdf/	  
+	Further information for parameter types: https://wetterdienst.readthedocs.io/_/downloads/en/latest/pdf/	  
+	If resolution has not a certain parameter: --> Atrribute error 
 	"""
 	observations = DWDObservationData(
-		station_ids = station_id,	#Stuttgart Schnarrenberg
+		station_ids = station_id,
 		parameters = weather_parameters,
 		resolution = resolution,
 		periods = periods,
@@ -54,9 +56,8 @@ def mergedCSVpattern(pattern_list, selected_cols=None, sep_char="[;,|]"):
 	for item in pattern_list:
 		## also look in the next subfolders
 		files = glob.glob("**/*{}*.csv".format(item), recursive=True)  ## get from cur. dir and all sub.dirs
-		pdb.set_trace()
-		df_combi = pd.concat([pd.DataFrame(pd.read_csv(f, sep=sep_char, engine="python", index_col=False), columns=selected_cols) for f
-			 in files], join="inner")  # join on identical col.names
+		#pdb.set_trace()
+		df_combi = pd.concat([pd.DataFrame(pd.read_csv(f, sep=sep_char, engine="python", index_col=False), columns=selected_cols) for f in files], join="inner")  # join on identical col.names
 		# index_col shouldnt be read in
 		# try to fetch multiple separor types, def engine="python" to not confuse it with regex separators, which can be read via c-engine
 		df_combi_list.append(df_combi)
@@ -64,21 +65,21 @@ def mergedCSVpattern(pattern_list, selected_cols=None, sep_char="[;,|]"):
 	return df_combi_list
 
 
-def merge_df2csv(left_dfs_list, right_dfs, col2merge_left, col2merge_right, outdir, outname):
+def merge_df2csv(left_dfs_list, right_df, col2merge_left, col2merge_right, outdir, outname):
 	"""
 	---- Merges pandas DataFrames according to identic column values and saves them as CSV ----
 	left_dfs_list: list of similar dataframes, column names and datatypes should be identic between the dataframes
-	right_df: single dataframe which should be merged, needs at least one identic column and datatype
+	right_df: single dataframe which should be merged, needs at least one identic column and datatype, rows which just occure in this dataframe (but not in the left dataframe) are ignored
 	outname : use a column element (eg. sensor id) to generate unique filenames
 	merge_pattern : column names with identical values and datatype in both dataframes (str)
-	returns list of merged dataframes and saves each as csv
+	returns list of merged dataframes and saves each dataframe as csv file
 	"""
 	df_all_list = []
 
 	for df in left_dfs_list:
-		df_merged = pd.merge(df, right_dfs, left_on=col2merge_left, right_on=col2merge_right)
+		df_merged = pd.merge(df, right_df, left_on=col2merge_left, right_on=col2merge_right)
 		df_all_list.append(df_merged)
-		pdb.set_trace()
+		#pdb.set_trace()
 		outname_sep = df_merged[outname][0]
 		fpath = os.path.join(outdir, "dataset_" + str(outname_sep) + ".csv")
 		df_merged.to_csv(fpath, sep=";")
@@ -86,7 +87,39 @@ def merge_df2csv(left_dfs_list, right_dfs, col2merge_left, col2merge_right, outd
 	return df_all_list
 
 
-## Idea from: https://www.geeksforgeeks.org/check-for-balanced-parentheses-in-python/
+def splitTrainTest_TS(df, split_date, feature_cols, target_col, standarize=False):
+	"""
+	--- Create Train and Test data based on a certain boundary date ----
+	df : pandas Dataframe which should be splitted
+	split_date : string object indicating the temporal boundary, format "YYYY-MM-DD HH:MM:SS"
+	feature_cols : list of strings indicating the column names which are used as features
+	target_col : string indicating the column which should be predicted
+	standarize : boolean indicating if feaures should be scaled to similar units, defaullts False
+	return four pandas DataFrames in the order of X_train, y_train, X_test, y_test
+	"""
+	X = df[feature_cols]
+	Y = df[target_col]
+	#preX_train, preX_test, y_train, y_test = train_test_split(X, Y, test_size=0.4, random_state=42)
+
+	if standarize == True:
+		### Standardize data due to different untis of the paramters/features (columns)
+		scaler = StandardScaler() 
+		X = pd.DataFrame(scaler.fit_transform(X),columns=X.columns) 
+
+	# create training and testing date by splitting by certain date
+	X_train = X.loc[:split_date] # before date
+	X_test = X.loc[split_date:] # after date
+
+	y_train = Y.loc[:split_date] 
+	y_test = Y.loc[split_date:]
+
+	print("Training input (rows, features): ", X_train.shape)
+	print("Training target: ", y_train.shape)
+	print("Testing input (rows, features): ", X_test.shape)
+	print("Testing target: ", y_test.shape)
+
+	return X_train, y_train, X_test, y_test
+
 
 
 def brackets(input_section):
